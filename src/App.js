65ba3c7d35b374812adafef1979411967caf3574
Flame2015 <ankur.das62@gmail.com>
2024-07-24 20:34:41 +0530

    finished ui

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { Container, Statistic, Header, Segment, Grid, Icon } from 'semantic-ui-react';
+import { Container, Statistic, Header, Segment, Grid, Icon, Form, Button } from 'semantic-ui-react';
 
 function App() {
   return (
@@ -55,7 +55,7 @@ function App() {
               Something
             </Grid.Column>
             <Grid.Column width={3} textAlign='right'>
-              $10.00
+              $40.00
             </Grid.Column>
             <Grid.Column width={3}>
               <Icon name='edit' bordered />
@@ -80,7 +80,57 @@ function App() {
           </Grid.Row>
         </Grid>
       </Segment>
+      <Segment color='red'>
+        <Grid columns={3} textAlign='right'>
+          <Grid.Row>
+            <Grid.Column width={10} textAlign='left'>
+              Something
+            </Grid.Column>
+            <Grid.Column width={3} textAlign='right'>
+              $30.00
+            </Grid.Column>
+            <Grid.Column width={3}>
+              <Icon name='edit' bordered />
+              <Icon name='trash' bordered />
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Segment>
+      <Segment color='green'>
+        <Grid columns={3} textAlign='right'>
+          <Grid.Row>
+            <Grid.Column width={10} textAlign='left'>
+              Something
+            </Grid.Column>
+            <Grid.Column width={3} textAlign='right'>
+              $20.00
+            </Grid.Column>
+            <Grid.Column width={3}>
+              <Icon name='edit' bordered />
+              <Icon name='trash' bordered />
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Segment>
 
+      <Header as="h3">Add New Transaction</Header>
+      <Form unstackable>
+        <Form.Group>
+          <Form.Input 
+            icon='tags'
+            width={12}
+            label='Description'
+            placeholder='New Shinny thing'
+            />
+            <Form.Input width={4} label='Value' placeholder='100.00' icon="dollar" iconPosition='left'></Form.Input>
+        </Form.Group>
+      </Form>
+
+      <Button.Group style={{marginTop:20}}>
+        <Button>Cancel</Button>
+        <Button.Or />
+        <Button primary>Ok</Button>
+      </Button.Group>
 
     </Container>
   );
